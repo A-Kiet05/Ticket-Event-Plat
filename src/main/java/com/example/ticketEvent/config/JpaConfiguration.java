32a110a9ea7055c57115bd5d093907a43ca917f1
Nguyen Tran Anh Kiet <akiet23062005@gmail.com>
2026-09-22 19:31:05 +0700

    set up configuratuion,filters and repository files

---
+++ src/main/java/com/example/ticketEvent/config/JpaConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.ticketEvent.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration 
+@EnableJpaAuditing 
+public class JpaConfiguration {
+    
+}
---
+++ src/main/java/com/example/ticketEvent/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.ticketEvent.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.ticketEvent.filters.UserProvisioningFilter;
+
+import jakarta.servlet.FilterChain;
+
+@Configuration
+
+public class SecurityConfig {
+    
+    @Bean 
+    public SecurityFilterChain filterChain(HttpSecurity http , UserProvisioningFilter userProvisioningFilter) throws Exception{
+
+        http
+            .authorizeHttpRequests(authorize -> 
+                authorize.anyRequest().authenticated())
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults() ))
+            .addFilterAfter( userProvisioningFilter , BearerTokenAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
